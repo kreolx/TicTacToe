314f0e37dc33cc5f462594f4f4e5007271242a5e
kreolx <kreolx@mail.ru>
2018-10-05 14:30:10 +1000

    game done without computer

---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public enum MarkType
+    {
+        Free,
+        /// <summary>
+        /// The cell is a O
+        /// </summary>
+        Naught,
+        /// <summary>
+        /// The cell is a X
+        /// </summary>
+        Cross
+    }
+}
